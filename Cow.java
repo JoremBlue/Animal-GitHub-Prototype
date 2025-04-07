fd88ac4e6d537540970c26d6ad56fd3e6ce696b5
Jorem Blue Bonador <135201717+JoremBlue@users.noreply.github.com>
2025-04-07 08:46:40 +0800

    Add files via upload

---
+++ Cow.java
@@ -0,0 +1,27 @@
+public class Cow implements Animal {
+    private int legs;
+    private String sound;
+    private String food;
+
+    public Cow(int legs, String sound, String food) {
+        this.legs = legs;
+        this.sound = sound;
+        this.food = food;
+    }
+
+    @Override
+    public Cow clone() {
+        return new Cow(legs, sound, food);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(getType() + " says: " + sound);
+    }
+
+
+    @Override
+    public String getType() {
+        return "Cow";
+    }
+}
